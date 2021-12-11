a66144966ce7c688d8d9b1ddfdb3ef0c3ecab2c6
Onkar Madhekar <onkarmadhekar22@gmail.com>
2021-12-11 16:19:47 +0530

    Added Card Design

---
+++ app/src/main/java/com/robust_binaries/paperback_harbor_app/BookCard.java
@@ -0,0 +1,14 @@
+package com.robust_binaries.paperback_harbor_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BookCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_card);
+    }
+}
